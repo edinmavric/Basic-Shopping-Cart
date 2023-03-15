14b3c8b5c508e9413377a5cfb523062f5aad2d36
Edin Mavric <edinmavric10@gmail.com>
2023-03-15 18:21:21 +0100

    added logic for cart

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,58 @@
-const addToCartBtn = document.querySelectorAll(".add-to-cart");
-const products = document.querySelectorAll(".product");
-const total = document.getElementById("total");
+const items = [
+  { name: "apple", price: 10 },
+  { name: "banana", price: 20 },
+  { name: "peach", price: 30 },
+];
+
+let cart = [];
+
+function addToCart(index) {
+  cart.push(items[index]);
+  displayCart();
+}
+
+function displayItems() {
+  const itemList = items
+    .map((item, index) => {
+      return `
+      <div style="padding: 20px; border: 1px solid orange;">
+      <p>${item.name} - $${item.price}</p>
+      <button onclick="addToCart(${index})">Add to Cart</button>
+      </div>
+      `;
+    })
+    .join("");
+  document.querySelector(".item-list").innerHTML = itemList;
+}
+
+function displayCart() {
+  const cartList = cart
+    .map((item) => {
+      return `
+    <div>
+    <p>${item.name} - $${item.price}</p>
+    </div>
+    `;
+    })
+    .join("");
+  document.querySelector(".cart-list").innerHTML = cartList;
+}
+
+displayItems();
+displayCart();
+
 const emptyCartBtn = document.getElementById("empty-cart");
+emptyCartBtn.addEventListener("click", () => {
+  let confirmEmpty = confirm("Are you sure you want to empty cart?");
+  if (confirmEmpty === true) {
+    document.querySelector(".cart-list").innerHTML = "";
+    cart = [];
+  }
+});
+
 const checkoutCartBtn = document.getElementById("checkout");
-const listCartItems = document.getElementById("list");
-const itemsPrice = document.querySelectorAll(".price");
-const addCartBtn = document.querySelectorAll(".add-to-cart");
+checkoutCartBtn.addEventListener("click", () => {
+  alert("Succesfull purchase, come back again!");
+  document.querySelector(".cart-list").innerHTML = "";
+  cart = [];
+});
